979f9f89cc4d7aacdcb87dea0fbb76542ec226b2
Ayofe Faheez <ayofefaheez@gmail.com>
2024-04-01 16:04:45 -0800

    styling completed

---
+++ script.js
@@ -0,0 +1,14 @@
+// const navContainer = document.querySelector('.nav--box');
+// const naviToggle = document.getElementById('navi-toggle');
+// const navButton = document.querySelector('.nav--button');
+// const homeSection = document.querySelector('.home');
+
+// const navContainerHeight = navContainer.offsetHeight;
+
+// navButton.addEventListener('click', function () {
+//   if (naviToggle.checked) {
+//     homeSection.style.transform = `translateY(${0}px)`;
+//   } else {
+//     homeSection.style.transform = `translateY(${navContainerHeight}px)`;
+//   }
+// });
